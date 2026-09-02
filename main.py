d08ab1a92728369d97aca0f594d27e87d90127ea
safid <safid2007@gmail.com>
2026-09-02 14:15:34 -0400

    fixed function and working on build_device

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-import sys
-from io import FileIO
 
+import sys
+sys.path.insert(0, '/usr/lib/python3.14/site-packages')
+from io import FileIO
 import hid
 import os
 import fcntl
-sys.path.insert(0, '/usr/lib/python3.14/site-packages')
+import struct
+
+
+
+def build_device_battery(vendor_id: int, product_id: int) -> hid.device:
+    """
+    Builds a hid.device object for the get_battery function
+    :param vendor_id:
+    :param product_id:
+    :return:
+    """
+    devices = hid.enumerate(vendor_id, product_id)
+    target = None
+    for d in devices:
+        if d['usage_page'] == 0xFF00:
+            target = d
+            break
+    dev = hid.Device(path=target['path'])
+    return dev
 
 
 def build_device(vendor_id: int, product_id: int) -> None | FileIO:
     """
+    Builds a __ to get the bytes for the lighting
     :param vendor_id: the vendor id of the mouse
     :param product_id: the product id of the mouse
     """
@@ -21,6 +41,7 @@ def build_device(vendor_id: int, product_id: int) -> None | FileIO:
             break
     if target is None:
         raise FileNotFoundError
+    print(open(target, 'rb+', buffering=0))
     return open(target, 'rb+', buffering=0)
 
 
@@ -47,12 +68,17 @@ def set_mode(device: hid.device) -> None:
     """
     #to be implemented
 
-
 VENDOR_ID = 0x258a
 PRODUCT_ID = 0x002f
 
-dev = build_device(VENDOR_ID, PRODUCT_ID)
+
+dev = build_device_battery(VENDOR_ID, PRODUCT_ID)
 get_battery(dev)
 
 
-# current = dev.get_feature_report(0x08, 528)
+dev = build_device(VENDOR_ID, PRODUCT_ID)
+
+
+size = 520
+HIDIOCGFEATURE = (0xC0000000 | (size << 16) | (0x48 << 8) | 0x07)
+print(hex(HIDIOCGFEATURE))
